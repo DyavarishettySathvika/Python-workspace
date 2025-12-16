29bc60d48dad9b9af9d50d30b737357eeb3d8b77
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2025-12-16 22:36:37 +0530

    16-12-2025

---
+++ Global variables.py
@@ -0,0 +1,31 @@
+x = "awesome"
+#Outside function
+def myfunc():
+  print("Python is " + x)
+myfunc()
+
+#Outside Function
+x = "Simple"
+def myfunc():
+  x = "Easy"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+#The Global Keyword
+#Outside Function
+def myfunc():
+  global x
+  x = "Keyword"
+myfunc()
+print("Python is " + x)
+
+#Outside Function
+x = "Language"
+def myfunc():
+  global x
+  x = "Interesting"
+myfunc()
+print("Python is " + x)
+
+
