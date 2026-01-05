35313a905ac44ceac507121e2882f194cdb3dc7f
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-01-05 23:08:12 +0530

    05-01-26

---
+++ Looping through String.py
@@ -0,0 +1,21 @@
+#->for loop
+for x in "Bunny":
+  print(x) 
+for x in 'Sathvika Dyavaishetty':
+  print(x)
+#It means when we use for the string print in a single alphabetical order
+
+#->String Length
+a = "Dyavarishetty Sathvika"
+print(len(a))
+#It Checks the String length how many letters were placed in a string. -> It is represented by len()
+b='St.Martins'
+print(len(b))
+c='College'
+print(len(c))
+
+#->Checking a Text of String
+name="Python is Popular Language"
+print("popular" in name)
+name="Brain is not braning"
+print("Python" not in name)
