d878634a6b654a1884cc7ea8b928dc1356d4e3d6
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-01-31 22:59:21 +0530

    31-01-2006

---
+++ Day1/Slicing Str.py
@@ -0,0 +1,27 @@
+#->Slicing String
+x = "Sathvika Dyavarishetty"
+print(x[5:19])
+# In Slicing string we need to determine by starting string number to ending of string number.This counting will start from 1 to so on
+y = "Dell"
+print(y[1:3])
+y="Slicing String"
+print(y[3:8])
+
+
+#->Slice from the Start
+a = "Sathvika"
+print(a[:5])
+# It will start from starting letter. It will count spacing of a string also.It will print starting values
+b="Vinit Nandhan"
+print(b[:6])
+
+#->Slice from the End
+a="St.Martins"
+print(a[4:])
+# . This symbol will also count a value.It will print ending values.
+b='Engineering'
+print(b[5:])
+
+#->Negative Indexing
+b = "Awesome!"
+print(b[-5:-2])
