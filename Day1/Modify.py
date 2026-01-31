528e3bd129efe19dc174ec4167bbfcbcdfffdd28
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-01-31 23:17:43 +0530

    31-01-2026

---
+++ Day1/Modify.py
@@ -0,0 +1,22 @@
+#->Uppercase
+a = "She is Beautiful!"
+print(a.upper())
+#In uppercase the letters will updated into big letters
+b='Modify the string'
+print(b.upper())
+
+#->Lowercase
+x='BACKSPace'
+print(x.lower())
+#In lowercase the letters will updated into small letters
+y='DeLEte'
+print(y.lower())
+
+#->Remove Whitespace
+text = "Boosting"
+Refresh= text.strip()
+print(Refresh)
+#In Removing whitespace we use .strip word to execute
+text = "Bagging"
+Refresh= text.strip()
+print(Refresh)
