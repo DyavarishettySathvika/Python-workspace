0a7685eed089b1a5bd763744e0db0dbd3cbf9e9b
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-01 23:17:21 +0530

    01-02-2026

---
+++ Day1/Escape Char.py
@@ -0,0 +1,7 @@
+#->Escape Character
+txt = "We started fitness from feb \"Walking\" and other activities can be held in fitness."
+print(txt) 
+# In this \ this symbol is escaped and printed without that symbol.It means escaping of letter occured
+
+txt="ACTUALLY WE SHOULD OUR CODING FROM 1ST YEAR WE GOT FOCUS AT \"3RD\" YEAR"
+print(txt)
