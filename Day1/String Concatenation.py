6fd78737dfb6abd49f14639ed33551495e237863
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-01 11:20:57 +0530

    01-02-2026

---
+++ Day1/String Concatenation.py
@@ -0,0 +1,19 @@
+#->String Concatenation means combing two strings.
+a="Data"
+b=" Type"
+c=a+b
+print(c)
+a="Quick"
+b="Sort"
+c=a+b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a+" "+b
+print(c)
+#a+" "+b the spaces determine space in the output
+x= 'Python'
+y= 'Workspace'
+z= x+ " " + y
+print(z)
