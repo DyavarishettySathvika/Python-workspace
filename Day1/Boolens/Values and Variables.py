771992747b37d531df3ba77afb61f1961e4d0292
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-02 20:47:51 +0530

    02-02-2026

---
+++ Day1/Boolens/Values and Variables.py
@@ -0,0 +1,19 @@
+#->Evaluating Values and Variables
+print(bool("Broh"))
+print(bool(14))
+
+#Giving X value and Y value
+x = "Broh"
+y = 14
+print(bool(x))
+print(bool(y))
+
+#Different symbols were used here
+print(bool(""))      # Empty string 
+print(bool("Hi"))    # Non-empty string 
+print(bool(0))       # Zero 
+print(bool(100))     # Non-zero 
+
+#By using list
+print(bool([]))      
+print(bool([1, 2]))  
