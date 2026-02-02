93349154a88ed683d031fd10a0b526e80c75e710
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-02 21:02:18 +0530

    02-02-2026

---
+++ Day1/Boolens/False.py
@@ -0,0 +1,12 @@
+#Some values are False 
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+result = (5 > 3) and (2 == 2)   
+result = (5 > 3) and (2 == 3)   
+print(bool(""))
+#If we give false values the output also gives false value only....
