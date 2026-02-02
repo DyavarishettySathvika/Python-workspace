39436951a0c2d0650b9a9e676f9d85c9ae6d3508
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-02 20:55:31 +0530

    02-02-2026

---
+++ Day1/Boolens/Trues.py
@@ -0,0 +1,14 @@
+#Most Values are True
+print(bool("abc"))
+print(bool(123))
+print(bool(["Never", "No", "Yes"]))
+print(bool(7))      
+print(bool(-4.5))   
+print(bool(True))     
+print(bool([1]))      
+print(bool({"a": 1})) 
+print(bool((0,)))     
+#If we give true values the output also gives true value only....
+
+
+
