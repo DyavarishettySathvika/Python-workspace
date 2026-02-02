9eda4da379a404798eaa794084c39ff59c36264d
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-02 23:43:08 +0530

    02-02-2026

---
+++ Day1/Boolens/Operations/Arithmetic.py
@@ -0,0 +1,19 @@
+#Arithmetic Operators
+x = 34
+y = 8
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#->Division in Python
+x = 23
+y = 8
+print(x / y)
+x = 12
+y = 5
+print(x // y)
+
