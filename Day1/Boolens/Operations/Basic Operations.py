4847db2934fb76b616d807798d8a98a6b1408cf9
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-02 23:30:18 +0530

    02-02-2026

---
+++ Day1/Boolens/Operations/Basic Operations.py
@@ -0,0 +1,28 @@
+#Operators are used to perform operations on variables and values.
+print(10+50)
+print(534-344)
+print(24*3)
+print(53/7)
+
+#->Using left hand sides
+sum1 = 100 + 50     
+sum2 = sum1 + 250   
+sum3 = sum2 + sum2   
+print(sum1)
+print(sum2)
+print(sum3)
+#->Substraction
+sum1 = 100 - 50     
+sum2 = sum1 - 250   
+sum3 = sum2 - sum2   
+print(sum1)
+print(sum2)
+print(sum3)
+#->Multiplication
+sum1 = 100 * 50     
+sum2 = sum1 * 250   
+sum3 = sum2 * sum2   
+print(sum1)
+print(sum2)
+print(sum3)
+
