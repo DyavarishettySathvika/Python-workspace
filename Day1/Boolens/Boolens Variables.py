5a93fac6bce42222291a00e2d14c430295d4e7a1
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-02 20:37:16 +0530

    02-02-2026

---
+++ Day1/Boolens/Boolens Variables.py
@@ -0,0 +1,27 @@
+#In Boolens Variables we specify two things True and False of a given statements.
+print(10 > 9)
+print(10 == 9)
+print(10<9)
+print(10 != 10)
+
+#->Giving X and Y values here
+x = True
+y = False
+print(x)  
+print(y)  
+
+#->We use if and else statements to clarify the boolens statement
+a = 96
+b = 22
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#->Checking Adult and not by using Boolen datatype
+age = 18
+if age >= 18:
+    print("Adult")
+else:
+    print("Not an adult")
+
