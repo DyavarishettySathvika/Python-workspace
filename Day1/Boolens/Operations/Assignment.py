3e0357b44d2b6a1519fa94548a6d0d36882ba4ed
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-03 22:38:53 +0530

    03-02-2026

---
+++ Day1/Boolens/Operations/Assignment.py
@@ -0,0 +1,66 @@
+#->Assignment operator assigns a value
+x = 5 #=Assignment operator
+print(x)
+
+#->Assigning 2 operators at a time.It means addition operator
+x = 5
+x += 3
+print(x)
+
+#->Assigning 2 operators at a time.It means substraction operator
+x = 5
+x -= 3
+print(x)
+
+#->Assigning 2 operators at a time.It means multiplication operator
+x = 5
+x *= 3
+print(x)
+
+#->Assigning 2 operators at a time.It means division operator
+x = 5
+x /= 3
+print(x)
+
+#->Assigning 2 operators at a time.It means Modulus operator
+x = 5
+x %= 3
+print(x)
+
+#->Assigning 2 operators at a time.It means Floor division operator
+x = 5
+x //= 3
+print(x)
+
+#->Assigning 2 operators at a time.It means Exponentinal operator
+x = 5
+x **= 3
+print(x)
+
+#->
+x = 5
+x &= 3
+print(x)
+
+#Using |=
+x = 5
+x |= 3
+print(x)
+
+#Using ^=
+x = 5
+x ^= 3
+print(x)
+
+#Using>>=
+x = 5
+x >>= 3
+print(x)
+
+#Using <<=
+x = 5
+x <<= 3
+print(x)
+
+#Using :=
+print(x := 3)
