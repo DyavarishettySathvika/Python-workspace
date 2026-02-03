f1cdc4d493a0b322c7533a6abdf9bb42819ca6d7
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-03 22:48:39 +0530

    03-02-2026

---
+++ Day1/Boolens/Operations/Comparsion.py
@@ -0,0 +1,50 @@
+#Comparison operators are used to compare two values
+#Using == 
+x = 5
+y = 3
+print(x == y)
+
+#Using != 
+x = 5
+y = 3
+print(x != y)
+
+#Using Greater than >
+x = 5
+y = 3
+print(x > y)
+
+#Using Less than <
+x = 5
+y = 3
+print(x < y)
+
+#Using Greater than or equal to >=
+x = 5
+y = 3
+print(x >= y)
+
+#Using Less than or equal to <=
+x = 5
+y = 3
+print(x <= y)
+
+#All together
+x = 22
+y = 87
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Chaining Comparsion 
+x = 10
+print(1 < x < 20)
+print(1 < x and x < 7)
+
+x=-15
+print(5>x<87)
+print(7<=7)
+
