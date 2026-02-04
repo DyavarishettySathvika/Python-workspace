4e21ff1d1309b29516b2d0cda2b5c42cfb12fd6a
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-04 23:00:48 +0530

    04-02-2026

---
+++ Day1/Boolens/Operations/Python Lists/List.py
@@ -0,0 +1,34 @@
+#List is represented in [] Lists are used to store multiple items in a single variable.
+mylist = ["lucky", "Sunny", "bunny"]
+#Using this, this means simply using word with this and without this the output will get same
+thislist = ["lucky", "bunny", "sunny"]
+print(thislist)
+
+#List Items
+#List items are ordered, changeable, and allow duplicate values.List items are indexed, the first item has index [0], the second item has index [1]
+#Ordered List
+# Creating an ordered list
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  
+print(fruits[1])  
+my_list = [1, "hello", 3.14, True]
+print(my_list)
+school = ["books","pens","bag"]
+print(school[1])
+print(school[2])
+
+#Changeable list
+# Create a list
+numbers = [10, 20, 30]
+print("Original list:", numbers)# Change an element
+numbers[1] = 99
+print("After change:", numbers)# Add a new element at the end
+numbers.append(40)
+print("After append:", numbers)
+
+#Allow Duplicates
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+my_list = [1, 2, 2, 3, 3, 3]
+print(my_list)
+
