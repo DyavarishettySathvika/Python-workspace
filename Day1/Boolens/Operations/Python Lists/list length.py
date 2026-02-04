3f1b5959feb3ebc248b30c0de7ccab5cd960a928
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-04 23:14:09 +0530

    04-02-2026

---
+++ Day1/Boolens/Operations/Python Lists/list length.py
@@ -0,0 +1,21 @@
+#List Length
+#To determine how many items a list has, use the len() function
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+my_list = [1, 2, 3, 4, 5]
+print(len(my_list))  
+fruits = ["apple", "banana", "cherry", "date"]
+print(len(fruits))  
+mixed = [1, "hello", True, [1, 2]]
+print(len(mixed)) 
+
+#List Items-Data Types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+list1 = ["abc", 34, True, 20, "female"]
+print(list1)
+#Here list of items are represented and printed here
