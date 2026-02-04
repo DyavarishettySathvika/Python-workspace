e3828373eda4e0a8a992ee0ea52e139db68519ca
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-04 22:34:29 +0530

    04-02-2026

---
+++ Day1/Boolens/Operations/Precedence.py
@@ -0,0 +1,38 @@
+#Operator Precedence
+#Operator precedence describes the order in which operations are performed by using (),+,-,*
+print((6 + 3) - (6 + 3))
+print((9+8)-(7-4))
+print(100 + 5 * 3)
+print(876-7/6)
+print(64^3+2)
+
+#Precedence Order
+#Parentheses Operator()
+print((4+7)-(5+7))
+
+#Exponentiation Operator **
+print(876-9**2)
+print(64+8**3)
+
+#Unary plus,Unary minus,and bitwise NOT +x,-x,~x
+print(244+~3)
+print(665-~4)
+
+#Multiplication,Division,Floor Division,and modulus
+print(864+2/4)
+print(76-86*7)
+print(35/4+2*5)
+print(34//2*4%9)
+
+#Addition and Substraction+-
+print(3+3)
+print(4-1)
+print(47+44-45)
+
+#Bitwise left and right shifts
+print(5>>7-7)
+print(5<<57+7)
+print(48+9/3)
+print(24+9*2>>2)
+
+#Here all the bitwise operators are listed again.
