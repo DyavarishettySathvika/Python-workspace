70c8355b0039c86862927180374e67221daf9340
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-06 22:42:19 +0530

    06-02-2026

---
+++ Day1/Boolens/Operations/Python Lists/Loop List.py
@@ -0,0 +1,10 @@
+#->Loop List
+#Loop through a list (by using for loop)
+thislist = ["Navya", "Manasa", "Bunny","Sathvika"]
+for x in thislist:
+  print(x)
+mylist = ["Choti", "Cutie", "Bunny","Lucky"]
+for x in mylist:
+  print(x)
+
+  
