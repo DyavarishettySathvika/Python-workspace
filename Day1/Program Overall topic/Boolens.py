325473b3c8931d9864b196bececaa0741cae3bf5
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-06 22:53:03 +0530

    06-02-2026

---
+++ Day1/Program Overall topic/Boolens.py
@@ -0,0 +1,27 @@
+a = True
+b = False
+print(a)  
+print(b)  
+#Comparison Operator
+x = 10
+y = 5
+print(x > y)   
+print(x == y)  
+print(x < y)   
+#Convert to Boolean (bool())
+print(bool(1))    
+print(bool(0))    
+print(bool("Hi")) 
+print(bool(""))   
+#Boolean in Conditions
+age = 18
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+#Using and, or, not
+is_student = True
+has_id = False
+print(is_student and has_id)  
+print(is_student or has_id)  
+print(not is_student)         
