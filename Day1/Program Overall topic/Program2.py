7857cdc0b24bde95b9bad38321cc900e02909374
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-07 22:14:58 +0530

    07-02-2026

---
+++ Day1/Program Overall topic/Program2.py
@@ -0,0 +1,14 @@
+# Add two numbers entered by the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum is:", a + b)
+# Loop through and print numbers
+for i in range(1, 6):
+    print(i)
+    num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
