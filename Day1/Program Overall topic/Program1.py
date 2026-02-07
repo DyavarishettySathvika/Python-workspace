ea3d8af8d3749d5f6d85531eed6cb93decaba4d1
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-07 22:09:26 +0530

    07-02-2026

---
+++ Day1/Program Overall topic/Program1.py
@@ -0,0 +1,14 @@
+print("Hello, Python Learner!")
+a = 10
+b = 5
+print("Sum of a and b is:", a + b)
+# Check if a number is even or odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
+# Countdown from 5 to 1
+for i in range(5, 0, -1):
+    print(i)
+print("Go!")
