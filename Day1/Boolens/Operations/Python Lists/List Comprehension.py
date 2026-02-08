e5602e09e335c4cd4f59fe04c49d459343d85df7
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-08 22:10:39 +0530

    08-02-2026

--- Day1/Boolens/Operations/Python Lists/List Comprehension.py
+++ Day1/Boolens/Operations/Python Lists/List Comprehension.py
@@ -12,3 +12,7 @@ for x in gadgets:
   if "a" in x:
     newlist.append(x)
 print(newlist)
+#Other Model
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
