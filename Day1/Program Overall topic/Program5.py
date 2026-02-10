338426321cb0dac1409567fe5319d51a744c3e6f
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-10 22:56:38 +0530

    10-02-2026

---
+++ Day1/Program Overall topic/Program5.py
@@ -0,0 +1,10 @@
+# Program to find the largest number in a list
+numbers = [12, 45, 2, 68, 33]
+
+largest = numbers[0]   # assume first is largest
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("The largest number is:", largest)
