2a46679011ed8d7cbaefb7981e173901078a04c1
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-11 22:21:04 +0530

    11-02-2026

---
+++ Day1/Boolens/Operations/Python Lists/Reverse Order.py
@@ -0,0 +1,15 @@
+#Reverse Order(the list will print in reverse order)
+list1=['Carrot','Rabbit','Fish','Grass']
+list1.reverse()
+print(list1)
+list2=['Tomato','Potato','Brinjal','Ladys Finger']
+list2.reverse()
+print(list2)
+
+#Printing numbers in reverse order
+list1=[2,3,6,85,56]
+list1.reverse()
+print(list1)
+list2=[764,97,44,86,756]
+list2.reverse()
+print(list2)
