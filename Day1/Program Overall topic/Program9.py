8e2f241fcfe5125f82f16e97cf5d747f725a69a9
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-12 22:56:45 +0530

    12-02-2026

---
+++ Day1/Program Overall topic/Program9.py
@@ -0,0 +1,12 @@
+# 1. Hello and your name
+print("Hello, World!")               # Print message
+name = input("Enter your name: ")    # Ask for user input
+print("Welcome,", name)              # Show greeting
+
+age = int(input("Enter your age: ")) # Get age as number
+if age >= 18:                        # Check eligibility
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+print("Thanks for using this simple program!")
