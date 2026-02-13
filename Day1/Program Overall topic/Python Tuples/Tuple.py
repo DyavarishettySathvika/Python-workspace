d059117992e9b22894aa8b23c98b5db5eaa8642b
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-13 22:36:10 +0530

    13-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Tuple.py
@@ -0,0 +1,8 @@
+#Defining a tuple same like list.Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.A tuple is a collection which is ordered and unchangeable.Tuples are written with round brackets
+tuple=("Goat",'Fish','Chicken','Mutton')
+mytuple=('List','Tuple','Boolen','Set')
+thistuple=('Apple','Banana','Grapes','Cherry')
+print(tuple)
+print(mytuple)
+print(thistuple)
+#Tuple Items,Ordered,Unchangeable,Allow Duplicates all are same like list only
