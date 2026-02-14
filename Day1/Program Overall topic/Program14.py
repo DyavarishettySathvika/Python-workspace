abcdbd58ec3e877a8cce012815ea77a97dbabee6
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-14 21:26:04 +0530

    14-02-2026

---
+++ Day1/Program Overall topic/Program14.py
@@ -0,0 +1,7 @@
+# Program 1: Create a tuple and access items
+
+students = ("Ram", "Sita", "John", "Priya")
+
+print("Full tuple:", students)
+print("First student:", students[0])
+print("Last student:", students[-1])
