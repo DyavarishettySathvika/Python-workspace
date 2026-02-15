46f783f0bd04e3e20b0964e08d5d6af6a3b50999
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-15 20:36:51 +0530

    15-02-2026

--- Day1/Program Overall topic/Program16.py
+++ Day1/Program Overall topic/Program16.py
@@ -6,3 +6,9 @@ tuple2 = (4, 5, 6)
 new_tuple = tuple1 + tuple2
 
 print("Joined tuple:", new_tuple)
+tuple1 = (4, 7, 2)
+tuple2 = (2, 4, 8)
+
+new_tuple = tuple1 + tuple2
+
+print("Joined tuple:", new_tuple)
