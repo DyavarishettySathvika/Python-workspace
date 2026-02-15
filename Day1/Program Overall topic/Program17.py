7a22d74ed864fec1db2f1ff57f868d7251dda9c6
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-15 20:34:40 +0530

    15-02-2025

--- Day1/Program Overall topic/Program17.py
+++ Day1/Program Overall topic/Program17.py
@@ -7,3 +7,10 @@ name, age, job = person
 print("Name:", name)
 print("Age:", age)
 print("Job:", job)
+person = ("Sathvika", 19, "Student")
+
+name, age, job = person
+
+print('Name:', name)
+print('Age:', age)
+print('Job:', job)
