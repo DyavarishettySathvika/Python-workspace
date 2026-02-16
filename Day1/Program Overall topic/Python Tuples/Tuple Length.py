3d650d5634d669823c5283c6c668e3a62ae14885
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-16 21:28:56 +0530

    16-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Tuple Length.py
@@ -0,0 +1,21 @@
+#Tuple Length(It determines how many items a tuple has, use the len() function.)
+tuple=("Manasa","Bunny","Navya","Harshitha")
+print(len(tuple))
+fruits=("Mango","Apple","Grapes","Cherry")
+print(len(fruits))
+#Tuple with Different Types
+set=('Sathvika',20,98.3,False)
+print(len(set))
+#Printing Numbers
+numbers=(3,5,6,78,3,1)
+print(len(numbers))
+#Empty Tuple Length
+empty=()
+print(len(empty))
+#One Tuple Length
+tuple=("Sathvika")
+print(len(tuple))
+#Tuple length using loop
+colors=('red','black','pink','green','blue')
+for i in range(len(colors)):
+    print(colors[i])
