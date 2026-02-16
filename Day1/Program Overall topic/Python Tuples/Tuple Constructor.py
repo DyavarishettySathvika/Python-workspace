ed27f348aca1b2bc710c479a837d785ee1bb43b8
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-16 22:00:32 +0530

    16-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Tuple Constructor.py
@@ -0,0 +1,9 @@
+#Tuple() Constructor
+thistuple = tuple(("Deru", "Yuku", "Lucky"))
+print(thistuple)
+thistuple = tuple(("Sathvika", "Rajesh", "Ramani"))
+print(thistuple)
+thistuple = tuple(("Rachana", "Varsha", "Abhi"))
+print(thistuple)
+thistuple = tuple(("Vamshi", "Shiva", "Chintu"))
+print(thistuple)
