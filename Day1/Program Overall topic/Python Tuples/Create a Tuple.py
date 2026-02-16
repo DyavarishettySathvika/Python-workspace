a33a80d42c77b71834227f7e7be9f2c987861970
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-16 21:35:52 +0530

    16-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Create a Tuple.py
@@ -0,0 +1,9 @@
+#Creating a Tuple(To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.)
+thistuple=('Sathvika,')
+print(type(thistuple))
+thistuple=('Bunny')
+print(type(thistuple))
+#Using Numericals
+tuple=10,
+print(tuple)
+print(type(tuple))
