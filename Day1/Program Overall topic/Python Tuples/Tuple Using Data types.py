99ce9cb2dccfdbab4f3515abcd2948845d23f8a7
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-16 21:43:03 +0530

    16-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Tuple Using Data types.py
@@ -0,0 +1,12 @@
+#Tuple using Data Types
+tuple1=('Apple','Mango','Cherry','Orange')
+tuple2=(5,8,5,2,8)
+tuple3=(True,False,True,False)
+tuple4=(65.9,56.9,34.9,98.7)
+print(tuple1)
+print(tuple2)
+print(tuple3)
+print(tuple4)
+#All togrther datatypes
+tuple1=('abc',78,True,4.8,"male")
+print(tuple1)
