dabdc434a2be9642f073b1945ddbceb97be0f030
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-17 19:18:20 +0530

    17-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Range of Negative Indexes.py
@@ -0,0 +1,16 @@
+#Range of Negative Indexes(Specify negative indexes if you want to start the search from the end of the tuple)
+fruits=('Mango','Pineapple','Apple','Orange','Kiwi','Cherry')
+print(fruits[-5:-2])
+print(fruits[-4:-1])
+print(fruits[-4:1])
+print(fruits[4:-1])
+print(fruits[-2:5])
+
+#Check if Item Exits
+tuple=('Sathvika','Bunny','Lucky','Vinay')
+if "Bunny" in tuple:
+   print("Yes, 'Bunny' is in the Cousin tuple")
+if "Teja" in tuple:
+   print("Yes, 'Teja' is in the Cousin tuple")
+if "Vinay" in tuple:
+   print("Yes, 'Vinay' is in the Cousin tuple")
