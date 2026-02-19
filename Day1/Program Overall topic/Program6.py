bf5ed60b3848a6137f620f00cf08205490bf732c
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-19 22:12:13 +0530

    10-02-2026

--- Day1/Program Overall topic/Program6.py
+++ Day1/Program Overall topic/Program6.py
@@ -1,5 +1,5 @@
 # Program to count how many times 'apple' appears
-fruits = ["apple", "banana", "apple", "orange", "apple"]
+fruits = ["apple", "grapes", "apple", "orange", "apple"]
 
 count = 0
 for fruit in fruits:
