bd394dfdb34f915f01ce35f8f5c5c6f9dca537ae
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-22 21:55:38 +0530

    22-02-2026

--- Day1/Program Overall topic/Program2.py
+++ Day1/Program Overall topic/Program2.py
@@ -3,7 +3,7 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Sum is:", a + b)
 # Loop through and print numbers
-for i in range(1, 9):
+for i in range(1, 2):
     print(i)
     num = int(input("Enter a number: "))
 if num > 0:
