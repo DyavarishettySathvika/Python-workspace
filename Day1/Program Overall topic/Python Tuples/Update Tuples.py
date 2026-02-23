0355ce521f3fa21f3c957aac8d975794ad37b09e
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-23 21:22:11 +0530

    23-02-2025

--- Day1/Program Overall topic/Python Tuples/Update Tuples.py
+++ Day1/Program Overall topic/Python Tuples/Update Tuples.py
@@ -1,2 +1,13 @@
 #Update Tuples(Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.)
 #Change Tuple Values()
+x=('Sathvika','Bunny','Lucky','Vinay')
+y=list(x)
+y[1]='Dyavarishetty'
+x=tuple(y)
+print(x)
+
+x=('Bag','Book','Pencil','Bottle')
+y=list(x)
+y[3]='Pen'
+x=tuple(y)
+print(x)
