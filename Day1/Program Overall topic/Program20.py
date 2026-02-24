03491858dfaa1e5739bd604a309951356dcd71c1
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-24 22:35:48 +0530

    24-02-2026

---
+++ Day1/Program Overall topic/Program20.py
@@ -0,0 +1,9 @@
+# Convert tuple to list
+tuple_data = (100, 200, 300)
+list_data = list(tuple_data)
+list_data.append(400)
+print("List from tuple:", list_data)
+
+# Convert list back to tuple
+new_tuple = tuple(list_data)
+print("New tuple:", new_tuple)
