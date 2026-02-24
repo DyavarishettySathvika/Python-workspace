30162ef464a801357051ad13aa204b3f35e40785
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-24 22:34:52 +0530

    24-02-2026

---
+++ Day1/Program Overall topic/Program19.py
@@ -0,0 +1,9 @@
+# List with different types
+my_list = [1, 2.5, "Hello", True]
+print("List:", my_list)
+print("List index 2:", my_list[2])  # Hello
+
+# Tuple with different types
+my_tuple = (1, 2.5, "World", False)
+print("Tuple:", my_tuple)
+print("Tuple index 1:", my_tuple[1])  # 2.5
