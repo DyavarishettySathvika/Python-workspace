e37d585d71f762936f79ff803f880f4f4ed4c78d
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-02-26 20:36:19 +0530

    26-02-2026

---
+++ Day1/Program Overall topic/Python Tuples/Unpack Tuples.py
@@ -0,0 +1,17 @@
+#Unpack Tuples(When we create a tuple, we normally assign values to it. This is called "packing" a tuple)
+branch=('IT','CSE','AIML','CSM','ECE','EEE')
+print(branch)
+
+food = ('Biryani','Fast food','Franky','Chicken')
+(Biryani,Fastfood,Franky,Chicken) =food
+print(Biryani)
+print(Fastfood)
+print(Franky)
+print(Chicken)
+
+nums=(23,4,23)
+x,y,z=nums
+print(x)
+print(y)
+print(z)
+
