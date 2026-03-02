d84647a20ed4c94cb1bba882d2cdf453cda1beb7
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-02 21:53:45 +0530

    02-03-2026

---
+++ Day1/Task Using Django/for loop.py
@@ -0,0 +1,11 @@
+playlist=['Song1','Song2','Song3']
+for song in playlist:
+    print(song)
+
+name=['Sathvika','Bunny','Lucky','Vinay']
+for names in name:
+    print(names)
+
+food=['Egg','Chicken','Mutton','Fish']
+for snacks in food:
+    print(snacks)
