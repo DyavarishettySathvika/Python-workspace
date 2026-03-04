5ef3704c58adeede8379de17926385ba5d5413f1
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-04 22:33:24 +0530

    04-03-2026

---
+++ Day1/Program Overall topic/Program28.py
@@ -0,0 +1,9 @@
+# Simple Python program in 10 lines
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello, " + name + "!")
+next_age = age + 1
+print("You are now", age, "years old.")
+print("Next year you will be", next_age, "years old.")
+for i in range(3):
+    print("Welcome to Python programming!")
