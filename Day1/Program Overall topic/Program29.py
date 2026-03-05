d437d80ab3d7b02df7f076071a828ec5e79ace1b
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-05 23:04:49 +0530

    05-03-2026

---
+++ Day1/Program Overall topic/Program29.py
@@ -0,0 +1,16 @@
+print("Find the Largest Number")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = a
+
+if b > largest:
+    largest = b
+
+if c > largest:
+    largest = c
+
+print("The largest number is:", largest)
+print("Program finished")
