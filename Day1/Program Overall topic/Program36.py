68d3ddb3cc1fb45ddf0905d26a1e0405fe804955
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-07 12:25:30 +0530

    07-03-2026

---
+++ Day1/Program Overall topic/Program36.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+i = 1
+while i <= 10:
+    result = num * i
+    print(num, "x", i, "=", result)
+    i = i + 1
+
+print("Table completed")
+print("Good job")
+print("End of program")
