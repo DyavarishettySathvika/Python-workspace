080231a6580c15cb6f6892999eb0a16d495a9db4
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-08 21:49:59 +0530

    08-03-2026

---
+++ Day1/Program Overall topic/Program38.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Numbers are:", a, b, c)
+
+if a > b and a > c:
+    largest = a
+elif b > c:
+    largest = b
+else:
+    largest = c
+
+print("Largest number is:", largest)
+
+print("Program finished")
