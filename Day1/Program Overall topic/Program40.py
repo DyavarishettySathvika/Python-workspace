750e5ce0a48c9e4eab564c60859ca6ba470ae1ed
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-08 21:53:47 +0530

    08-03-2026

---
+++ Day1/Program Overall topic/Program40.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+print("Multiplication table of", num)
+
+for i in range(1, 11):
+
+    result = num * i
+
+    print(num, "x", i, "=", result)
+
+print("Table completed")
+
+print("End of program")
