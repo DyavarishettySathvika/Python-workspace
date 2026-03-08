c3c5c346e3e59ed764c8958adad1368e28ca3e9e
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-08 21:51:24 +0530

    08-03-2026

---
+++ Day1/Program Overall topic/Program39.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+fact = 1
+
+for i in range(1, num + 1):
+
+    fact = fact * i
+
+print("Number is:", num)
+
+print("Factorial is:", fact)
+
+print("Calculation done")
