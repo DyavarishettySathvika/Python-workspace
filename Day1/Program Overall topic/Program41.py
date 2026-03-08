c6b7d7dde8273c7d32e498c431b489dc8c6a7e2a
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-08 21:56:21 +0530

    08-03-2026

---
+++ Day1/Program Overall topic/Program41.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+print("You entered:", num)
+
+if num % 2 == 0:
+    print("Checking condition")
+    print("Number is divisible by 2")
+    print("So it is Even")
+else:
+    print("Checking condition")
+    print("Number is not divisible by 2")
+    print("So it is Odd")
+
+print("Program completed")
