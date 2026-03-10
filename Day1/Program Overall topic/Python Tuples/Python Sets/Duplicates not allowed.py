66e2e10c24e76334fff54deec14f546b3895473c
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-10 21:45:14 +0530

    10-03-2026

---
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Duplicates not allowed.py
@@ -0,0 +1,10 @@
+thisset={'apple','carrot','carrot','beetroot'}
+print(thisset)
+
+#True and 1 is considered the same value
+thisset = {'camel','horse','zebra',True,1}
+print(thisset)
+
+#False and 0 is considered the same value:
+thisset = {'white','black','pink','yellow',False,0,1}
+print (thisset)
