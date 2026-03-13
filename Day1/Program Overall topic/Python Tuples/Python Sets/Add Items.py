1d12f2cddb6350fac065e53c538451a2870e549f
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-13 22:58:31 +0530

    13-03-2026

---
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Add Items.py
@@ -0,0 +1,12 @@
+thisset = {'Bunny','Lucky','Vinay'}
+thisset.add('Teja')
+print(thisset)
+
+thisset = {'Apple','Banana','Grapes'}
+thisset.add('Orange')
+print(thisset)
+
+thisset = {'One','Two','Three'}
+thisset.add('Four')
+print(thisset)
+
