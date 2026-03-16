114234ddc2308828bacb523b7894092b89d443a0
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-16 20:44:41 +0530

    16-03-2026

---
+++ Day1/Program Overall topic/Program48.py
@@ -0,0 +1,15 @@
+# Program to add two numbers
+
+print("Addition of two numbers")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("First number:", num1)
+print("Second number:", num2)
+
+print("Sum =", sum)
+
+print("Program completed")
