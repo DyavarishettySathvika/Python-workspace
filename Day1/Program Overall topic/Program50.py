28c64f0b3fd59a3cdae54eaef0a9318548996a9d
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-16 20:46:43 +0530

    16-03-2026

---
+++ Day1/Program Overall topic/Program50.py
@@ -0,0 +1,14 @@
+# Program to find factorial
+
+num = int(input("Enter a number: "))
+
+fact = 1
+
+for i in range(1, num + 1):
+    fact = fact * i
+
+print("Number:", num)
+
+print("Factorial:", fact)
+
+print("Calculation completed")
