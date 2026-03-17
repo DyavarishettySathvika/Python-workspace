70f6ef85249a10271200cb76f55e01ec86176e1f
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-17 22:40:51 +0530

    17-03-2026

---
+++ Day1/Program Overall topic/Program54.py
@@ -0,0 +1,14 @@
+num = int(input("Enter number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime Number")
+else:
+    print("Not Prime")
+
+print("Checked successfully")
+print("End of program")
