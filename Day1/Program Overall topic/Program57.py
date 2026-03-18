4831348a8b9761f9f925989dba60d7c7c8d57c72
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-18 20:57:52 +0530

    18-03-2026

---
+++ Day1/Program Overall topic/Program57.py
@@ -0,0 +1,14 @@
+# Check even or odd
+print("Even or Odd Checker")
+
+num = int(input("Enter a number: "))
+
+print("You entered:", num)
+
+if num % 2 == 0:
+    print("It is an Even number")
+else:
+    print("It is an Odd number")
+
+print("Checking completed")
+print("Thank you!")
