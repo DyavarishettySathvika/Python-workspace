216959a6bed0f0d38f3eadca368c9333829865ee
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-20 19:22:20 +0530

    20-03-2026

---
+++ Day1/Program Overall topic/Program62.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+length = int(input("Enter password length: "))
+chars = string.ascii_letters + string.digits + "!@#$%"
+
+password = ""
+for i in range(length):
+    password += random.choice(chars)
+
+print("Generated Password:", password)
