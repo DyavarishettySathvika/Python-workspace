19cae8228957b1474ab0cc13852a3781ff4372ec
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-26 21:42:40 +0530

    26-03-2026

--- Day1/Program Overall topic/Python Tuples/Python Sets/Remove Item.py
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Remove Item.py
@@ -1,3 +1,17 @@
 thisset={'sathvika','bunny','lucky'}
 thisset.remove('bunny')
 print(thisset)
+
+thisset={'one','two','three'}
+thisset.discard('three')
+print(thisset)
+
+thisset={'five','six','seven'}
+x=thisset.pop()
+print(x)
+print(thisset)
+
+thisset={'eight','nine','ten','eleven'}
+x=thisset.pop()
+print(x)
+print(thisset)
