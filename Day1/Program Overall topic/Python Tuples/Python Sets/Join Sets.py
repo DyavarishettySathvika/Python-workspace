a70309b64faf3aeb920ae761d7df13f750c28d6a
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-27 22:57:18 +0530

    27-03-2026

---
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Join Sets.py
@@ -0,0 +1,21 @@
+#Union (It joins two sets it means combination of sets)
+s1={'s','a','t','h',}#It means the values presented in the set will randomly assigned beacause it is unordered 
+s2={'v','i','k','a'}
+s3=s1.union(s2)
+print(s3)
+
+a={1,3,5,7,9}
+b={2,4,6,8,10}
+c=a.union(b)
+print(c)
+
+#Other Method
+set1={'a','b','c','d'}
+set2={2,5,9,1}
+set3=set1|set2
+print(set3)
+
+set1={'lock'}
+set2={'down'}
+set3=set1|set2
+print(set3)
