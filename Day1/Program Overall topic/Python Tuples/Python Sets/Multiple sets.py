985cffd452d9fdc2f64e210f2ee31a02d25838a1
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-27 23:13:55 +0530

    27-03-2026

---
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Multiple sets.py
@@ -0,0 +1,13 @@
+s1={'a','b','c','d','e'}
+s2={1,2,3,4,5}
+s3={'bunny','lucky'}
+s4={'sri','rama','navami'}
+myset=s1.union(s2,s3,s4)
+print(myset)
+
+s1={123,564,234,287}
+s2={'bunny','lucky','vinay'}
+s3={'chitti','chinnu','thinku'}
+myset=s1|s2|s3
+print(myset)
+
