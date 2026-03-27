05be14eaf57e59112a788f93d7588f5d11fc114e
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-03-27 22:26:36 +0530

    27-03-2026

---
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Loop Sets.py
@@ -0,0 +1,8 @@
+#Loop Items(By using for loop) Here the given input will print in a sequence order
+thisset = {'Sri','Rama','Navami'}#It means the n is the starting letter compared to others
+for x in thisset:
+    print(x)
+
+thisset={3,4,5,7,2,4}
+for y in thisset:
+    print(y)
