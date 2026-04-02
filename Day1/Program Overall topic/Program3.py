af6fb95bfab849b328837965c605cd1b8c0c9172
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-04-02 21:46:45 +0530

    02-03-20

--- Day1/Program Overall topic/Program3.py
+++ Day1/Program Overall topic/Program3.py
@@ -3,7 +3,7 @@ fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print("I like", fruit)
 # Find max of three numbers
-a = 9
-b = 2
-c = 5
+a = 355
+b = 35
+c = 76
 print("Maximum is", max(a, b, c))
