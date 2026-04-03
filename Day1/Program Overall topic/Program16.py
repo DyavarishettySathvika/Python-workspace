a02d16e456cb99311782ca0b0d354004ce0949c4
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-04-03 23:38:57 +0530

    show

--- Day1/Program Overall topic/Program16.py
+++ Day1/Program Overall topic/Program16.py
@@ -1,13 +1,13 @@
 # Program 3: Join two tuples
 
-tuple1 = (8,1,4)
-tuple2 = (4,8,3)
+tuple1 = (6,5,4)
+tuple2 = (4,5,6)
 
 new_tuple = tuple1 + tuple2
 
 print("Joined tuple:", new_tuple)
-tuple1 = (7,5,2)
-tuple2 = (5,8,2)
+tuple1 = (7,5,4)
+tuple2 = (5,7,4)
 
 new_tuple = tuple1 + tuple2
 
