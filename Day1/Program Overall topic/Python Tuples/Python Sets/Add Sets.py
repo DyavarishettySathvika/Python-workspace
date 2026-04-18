e3b9bf012d6399b097ca4c6f5da9a405fcbdfb6f
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-04-18 20:41:54 +0530

    k

--- Day1/Program Overall topic/Python Tuples/Python Sets/Add Sets.py
+++ Day1/Program Overall topic/Python Tuples/Python Sets/Add Sets.py
@@ -1,5 +1,5 @@
 #Add Sets
 thisset = {'Apple','Banana','Grapes','Cherry'}
-tropical = {'Tomato','grapes','papaya'}
+tropical = {'Tomato','grapes','papayaa'}
 thisset.update(tropical)
 print(thisset)
