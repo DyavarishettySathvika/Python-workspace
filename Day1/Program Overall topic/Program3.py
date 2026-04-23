5c868c84e8ecb9931fd2e093cefb065828b0d09e
DyavarishettySathvika <dyavarishettysathvika@gmail.com>
2026-04-23 09:21:03 +0530

    er

--- Day1/Program Overall topic/Program3.py
+++ Day1/Program Overall topic/Program3.py
@@ -1,5 +1,5 @@
 # Working with a list
-fruits = ["apple", "straw berries", "cherries"]
+fruits = ["apple", "grapes", "cherries"]
 for fruit in fruits:
     print("I like", fruit)
 # Find max of three numbers
